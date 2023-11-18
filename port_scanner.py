61cc7ab33803039fbf6101db9fccbdfd07becfd3
Vinaychowdarylella <67087974+Vinaychowdarylella@users.noreply.github.com>
2023-11-18 11:28:10 -0500

    Update port_scanner.py

--- port_scanner.py
+++ port_scanner.py
@@ -1,62 +1,100 @@
 import nmap
 import sys
 import logging
-import time
+import os
+import argparse
+import threading
 import random
+import time
 
-def initiate_stealthy_scan(scanner, target, ports):
-    """Perform a stealthy Nmap scan with enhanced OS detection."""
+# Predefined Nmap script arguments for enhanced scanning
+SCRIPT_ARGS = "default,vuln"
+
+# Settings for stealthier and parallel scanning
+MAX_THREADS = 5
+SCAN_DELAY = 2  # Delay in seconds between individual port scans
+
+def get_next_filename(prefix="scan", suffix=".txt", directory="scan_results"):
+    """Generates the next available filename for saving scan results."""
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    
+    counter = 0
+    while True:
+        filename = os.path.join(directory, f"{prefix}{counter}{suffix}")
+        if not os.path.exists(filename):
+            return filename
+        counter += 1
+
+def write_scan_results_to_file(scanner, filename, is_single_port):
+    """Writes the results of the Nmap scan to a file."""
+    with open(filename, "w") as file:
+        for host in scanner.all_hosts():
+            file.write(f'Host : {host} ({scanner[host].hostname()})\n')
+            file.write(f'State : {scanner[host].state()}\n')
+
+            if 'osclass' in scanner[host]:
+                os_detail = f"{scanner[host]['osclass'][0]['osfamily']} {scanner[host]['osclass'][0]['osgen']} {scanner[host]['osclass'][0]['accuracy']}%"
+            else:
+                os_detail = 'OS details not available'
+            file.write(f'OS Details: {os_detail}\n')
+
+            for proto in scanner[host].all_protocols():
+                file.write(f'----------\nProtocol : {proto}\n')
+                lport = sorted(scanner[host][proto].keys())
+                for port in lport:
+                    port_info = scanner[host][proto][port]
+                    state = port_info['state']
+                    if state in ['open', 'filtered'] or (state == 'closed' and is_single_port):
+                        service_info = f"{port_info.get('name', 'Unknown')} {port_info.get('product', '')} {port_info.get('version', '')}"
+                        port_data = f'Port : {port}\tState : {state}\tService : {service_info}\n'
+                        file.write(port_data)
+
+def initiate_scan(scanner, target, port, traceroute=False):
     try:
-        logging.info("Starting Nmap scan with enhanced OS detection. This may take a while...")
+        scan_arguments = '-sS -T2 -O -sV --osscan-guess --max-os-tries 1 --script=' + SCRIPT_ARGS
+        if traceroute:
+            scan_arguments += ' --traceroute'
 
-        # Adjusted arguments for OS detection and stealth
-        scan_arguments = '-sS -T2 -O --osscan-guess --max-os-tries 1'
-        
-        # Randomize port order and slow down the scan
-        port_list = ports.split(',')
-        random.shuffle(port_list)
-        for port in port_list:
-            scanner.scan(target, port, arguments=scan_arguments)
-            time.sleep(random.uniform(10, 20))  # Random sleep between scans of individual ports
-
-        logging.info("Nmap scan completed.")
+        scanner.scan(target, port, arguments=scan_arguments)
+        time.sleep(SCAN_DELAY)  # Adding delay between scans
     except nmap.PortScannerError as e:
         logging.error(f"Nmap scan error: {e}")
-        sys.exit(1)
     except Exception as e:
         logging.error(f"Unexpected error: {e}")
-        sys.exit(1)
-
-def print_scan_results(scanner):
-    """Prints the results of the Nmap scan."""
-    for host in scanner.all_hosts():
-        logging.info(f'Analyzing host: {host}')
-        print(f'Host : {host} ({scanner[host].hostname()})')
-        print(f'State : {scanner[host].state()}')
-
-        os_detail = scanner[host]['osclass'] if 'osclass' in scanner[host] else 'OS details not available'
-        print(f'OS Details: {os_detail}')
-
-        for proto in scanner[host].all_protocols():
-            print(f'----------\nProtocol : {proto}')
-
-            lport = sorted(scanner[host][proto].keys())
-            for port in lport:
-                port_info = scanner[host][proto][port]
-                service_info = f"{port_info.get('name', 'Unknown')} {port_info.get('product', '')} {port_info.get('version', '')}"
-                print(f'Port : {port}\tState : {port_info["state"]}\tService : {service_info}')
 
 def main():
+    parser = argparse.ArgumentParser(description='Perform an Nmap scan on a specified target.')
+    parser.add_argument('target', help='IP or hostname to scan')
+    parser.add_argument('-p', '--port', help='Specific port(s) to scan (comma-separated, no spaces)', default="1-65535")
+    parser.add_argument('-t', '--traceroute', action='store_true', help='Perform traceroute')
+    args = parser.parse_args()
+
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
     nm = nmap.PortScanner()
+    target = args.target
+    ports = [p.strip() for p in args.port.split(',')]  # Split the ports by comma and strip spaces
+    is_single_port = len(ports) == 1 and '-' not in ports[0]
+    traceroute = args.traceroute
+    filename = get_next_filename()
 
-    target = input("Enter the target IP or hostname to scan: ")
-    ports = input("Enter port range (e.g., '1-1024'): ")
+    logging.info(f"Preparing to scan {target}. Results will be saved in {filename}.")
 
-    logging.info(f"Preparing to scan {target} on ports {ports}.")
-    initiate_stealthy_scan(nm, target, ports)
-    print_scan_results(nm)
+    random.shuffle(ports)  # Randomize the port order
+
+    threads = []
+    for port in ports:
+        while threading.active_count() > MAX_THREADS:
+            time.sleep(1)  # Wait if the maximum number of threads is reached
+        thread = threading.Thread(target=initiate_scan, args=(nm, target, port, traceroute))
+        thread.start()
+        threads.append(thread)
+
+    for thread in threads:
+        thread.join()
+
+    write_scan_results_to_file(nm, filename, is_single_port)
 
 if __name__ == "__main__":
     main()
